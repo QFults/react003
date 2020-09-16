0a0503f8e7c105a3f601e1901385bda9103d356b
Quinton Fults <quinton.fults.work@gmail.com>
2020-09-16 11:14:54 -0700

    code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import React, { useState } from 'react'
 const App = () => {
 
   const [countState, setCountState] = useState({
-    count: 0
+    count: 0,
+    message: 'Hello'
   })
 
-  const handleBtnClick = () => {
-    setCountState({ count: countState.count + 1 })
+  countState.handleIncrement = () => {
+    setCountState({ ...countState, count: countState.count + 1 })
+  }
+
+  countState.handleDecrement = () => {
+    setCountState({ ...countState, count: countState.count - 1 })
+  }
+
+  countState.handleReset = () => {
+    setCountState({ ...countState, count: 0 })
   }
 
   return (
     <>
+      <h1>{countState.message}</h1>
       <h1>Count: {countState.count}</h1>
-      <button onClick={handleBtnClick}>Click Me</button>
+      <button onClick={countState.handleIncrement}>+</button>
+      <button onClick={countState.handleDecrement}>-</button>
+      <button onClick={countState.handleReset}>RESET</button>
     </>
   )
 }
